d1961f0e5dc27a6fa559e5e25c8d79c4656b2b1b
kabeiro <kabeiro2@gmail.com>
2021-02-23 17:04:24 +0000

    python for everybody, ex 12

---
+++ Python-for-Everybody/ex-12-2-html-scraping.py
@@ -0,0 +1,31 @@
+"""
+Write a program that will use urllib to read the HTML from the data files below, 
+and parse the data, extracting numbers and compute the sum of the numbers in the file. 
+"""
+
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+count = 0
+
+# Retrieve all the spans
+tags = soup('span')
+for tag in tags:
+    # Convert the number to integer and sum together
+    newNum = int(tag.contents[0])
+    count += newNum
+
+print(count)
